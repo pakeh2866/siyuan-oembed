9f10e93efa944874be3bbcd3c4a8d5cf3661684f
Anarion <anarion80@gmail.com>
2024-10-31 16:56:51 +0100

    🔊 Add plugin name to logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,3 +1,5 @@
+import { i18n } from "@/i18n";
+
 export const logSuccess = (operation: string) => console.log(`${operation} completed successfully`);
 
 export const logError = (operation: string, error: unknown) => console.error(`Error during ${operation}:`, error);
@@ -22,20 +24,20 @@ class Logger {
 
     debug(message: string, ...optionalParams: any[]): void {
         if (settings.debug) {
-            console.debug(`[DEBUG] ${message}`, ...optionalParams);
+            console.debug(`[DEBUG][${i18n.name}] ${message}`, ...optionalParams);
         }
     }
 
     info(message: string, ...optionalParams: any[]): void {
-        console.info(`[INFO] ${message}`, ...optionalParams);
+        console.info(`[INFO][${i18n.name}] ${message}`, ...optionalParams);
     }
 
     warn(message: string, ...optionalParams: any[]): void {
-        console.warn(`[WARN] ${message}`, ...optionalParams);
+        console.warn(`[WARN][${i18n.name}] ${message}`, ...optionalParams);
     }
 
     error(message: string, ...optionalParams: any[]): void {
-        console.error(`[ERROR] ${message}`, ...optionalParams);
+        console.error(`[ERROR][${i18n.name}] ${message}`, ...optionalParams);
     }
 }
 
